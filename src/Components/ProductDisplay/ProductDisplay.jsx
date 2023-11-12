d1cdf4e3619be01a0628b12625d119401c73889c
shRamim13 <shramim13@gmail.com>
2023-11-12 11:07:42 +0600

    2nd commit

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png'
 import star_dull_icon from '../Assets/star_dull_icon.png'
+import { ShopContext } from '../../Context/ShopContex'
 
 const ProductDisplay = (props) => {
     const { product } = props;
+    const { addToCart } = useContext(ShopContext)
     return (
         <div className='productdisplay'>
             <div className="productdisplay-left">
